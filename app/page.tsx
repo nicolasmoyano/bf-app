a7b744e51af821f3ecfd66eed7f8309d744eef5a
nicolasmoyano <info@nicolas-moyano.se>
2024-11-21 14:38:55 +0100

    added feature component to Startpage

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,7 @@ export default function Home() {
     <div className="bg-slate-950 pt-10">
       <Hero />
       <Feature />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex">
-          <Feature />
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-      </div>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
     </div>
   );
 }
